ac050f7724ff672486cc334f50a65361ace263c0
Zac <zombiezac@att.net>
2018-09-24 13:33:15 -0600

    variables and lists

--- First.playground/Contents.swift
+++ First.playground/Contents.swift
@@ -5,3 +5,39 @@ import UIKit
 var firstString = "Hello, playground"
 
 print(firstString)
+
+var MyVariable = 42
+MyVariable = 50
+let MyConstant = 42
+
+let implicictInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples"
+let orangeSummary = "I have \(oranges) oranges"
+
+var shoppingList = ["catfish", "water", "tulips"]
+shoppingList[1] = "bottle of water"
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+
+occupations["Jayne"] = "PublicRelations"
+
+shoppingList.append("blue paint")
+shoppingList.append("garbage can")
+shoppingList.append("the jews")
+
+print(shoppingList)
+
+
+
